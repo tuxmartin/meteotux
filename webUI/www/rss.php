a9efd9e67813e9825115c7748756952ff9aac745
Martin Vancl <tux.martin@gmail.com>
2013-07-15 17:01:35 +0200

    hodnoty z TMEP tabulky; prechod na RSS2.0, vyhazeni zbytecnych elementu (mensi prenos dat)

--- webUI/www/rss.php
+++ webUI/www/rss.php
@@ -2,13 +2,13 @@
 <?php
 header('Expires: ' . gmdate('D, d M Y H:i:s') . '  GMT');
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . '  GMT');
-header('Content-Type: text/xml; charset=utf-8');
+header('Content-Type: application/rss+xml; charset=utf-8');
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Pragma: no-cache");
 
 $dotaz=mysql_query(
-	"SELECT teplota, vlhkost FROM Hodnoty ORDER BY casMereni DESC LIMIT 1"
-	) or die("CHYBA MySQL: " . mysql_error());
+		"SELECT teplota, vlhkost FROM tme ORDER BY id DESC LIMIT 1"
+		) or die("CHYBA MySQL: " . mysql_error());
 
 $zaznam=MySQL_Fetch_Array($dotaz);
 
@@ -19,33 +19,27 @@ elseif ($_GET["p"] == "v") {
 	$vypsat = $zaznam["vlhkost"] . "%";
 }
 else {
-	$vypsat = $zaznam["teplota"] . "°C " . $zaznam["vlhkost"] . "%";	
+	$vypsat = $zaznam["teplota"] . "°C " . "\n" . $zaznam["vlhkost"] . "%";	
 }
 	
 ?>
-<rss version="0.91">
-	<channel>
-		<title>Počasí v Jičíně</title>
-		<link>http://localhost/martin/teplota/www/</link>
-		<description>Počasí v Jičíně</description>
-		<language>cs</language>
-		<image>
-			<url></url>
-			<title>Počasí v Jičíně</title>
-			<link/>
-		</image>
-		<item><?php echo $vypsat?>
-			<title><?php echo $vypsat?></title>
-			<link>http://teplomer.jicin.cz/</link>
-			<description>http://localhost/martin/teplota/www/</description>
-			<pubDate><?php echo gmdate("D, d M Y H:i:s")." GMT";  ?></pubDate>
-		</item>
-	</channel>
+<rss version="2.0">
+  <channel>
+    <title></title>
+    <link></link>
+    <description></description>
+    <pubDate><?php echo gmdate("D, d M Y H:i:s")." GMT";  ?></pubDate>
+    <item>
+       <title><?php echo $vypsat?></title>
+    </item>
+  </channel>
 </rss>
 
 <?php
 /*
-Na Androidu:
+specifikace povinnych RSS elementu: http://feed2.w3.org/docs/rss2.html
+
+Androidu widget:
 https://play.google.com/store/apps/details?id=de.j4velin.rssWidget
 https://dl.dropbox.com/u/6943408/teplomerRSS.png
 */
