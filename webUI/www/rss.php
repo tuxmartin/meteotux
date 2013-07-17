5c2b340aa6f3bb373ce4ec3d86dc99dcda176eac
Martin Vancl <tux.martin@gmail.com>
2013-07-17 22:47:31 +0200

    +ve verzi bez parametru vraci RSS cas (hodiny a minuty)

--- webUI/www/rss.php
+++ webUI/www/rss.php
@@ -19,7 +19,7 @@ elseif ($_GET["p"] == "v") {
 	$vypsat = $zaznam["vlhkost"] . "%";
 }
 else {
-	$vypsat = $zaznam["teplota"] . "°C " . "\n" . $zaznam["vlhkost"] . "%";	
+	$vypsat = $zaznam["teplota"] . "°C " . "\n" . $zaznam["vlhkost"] . "%" . "\n" . date("H:i");
 }
 	
 ?>
